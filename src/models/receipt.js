374aa77adfb4c7afe4fca23caab48a1f3b03e2c4
yhl221 <1727538058@qq.com>
2016-07-25 20:53:15 +0800

    creat receipt.js

---
+++ src/models/receipt.js
@@ -0,0 +1,9 @@
+class Receipt {
+  constructor(receiptItems, total, savedTotal) {
+    this.receiptItems = receiptItems;
+    this.total = total;
+    this.savedTotal = savedTotal;
+  }
+}
+
+modelu.exports = Receipt;
