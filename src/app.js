e5a0eec2924e48c0f0e12daa25cce99ba757cba8
yhl221 <1727538058@qq.com>
2016-07-25 21:16:46 +0800

    move loadAllItems to item.js replace with Item.all method

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ const loadAllItems = fixture.loadAllItems;
 const loadPromotions = fixture.loadPromotions;
 const CartItem = require('./models/cart-item.js');
 const ReceiptItem = require('./models/receipt-item.js');
-const Receipt=require('./models/receipt.js');
+const Receipt = require('./models/receipt.js');
+const Item = require('./models/item.js');
 
 function printReceipt(tags) {
-
-  const allItems = loadAllItems();
-  const cartItems = buildCartItems(tags, allItems);
+  
+  const cartItems = buildCartItems(tags, Item.all());
 
   const allPromotions = loadPromotions();
   const receiptItems = buildReceiptItems(cartItems, allPromotions);
