83a4d66f6fddeb9deb2741c6d9efd9dfd916b729
yhl221 <1727538058@qq.com>
2016-07-25 22:08:07 +0800

    move bildReceiptText to receipt.js replace Receipt.buildReceiptText method

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -1,4 +1,5 @@
 class Receipt {
+
   constructor(receiptItems, total, savedTotal) {
     this.receiptItems = receiptItems;
     this.total = total;
@@ -17,6 +18,32 @@ class Receipt {
 
     return new Receipt(receiptItems, total, savedTotal);
   }
+
+
+  static buildReceiptText() {
+
+    const formatMoney = (money)=> {
+      return money.toFixed(2);
+    };
+
+    let receiptItemsText = this.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
+  };
+
 }
 
 module.exports = Receipt;
